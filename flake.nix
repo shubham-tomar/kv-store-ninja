51f50bb6a4f196d707c00157d44fbac023ad3a3f
shubham.tomar <shubham.tomar@juspay.in>
2024-05-31 18:13:30 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "kv-store-ninja";
+
+  nixConfig.bash-prompt = "[nix]λ ";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          rust-overlay.overlay
+          naersk.overlay
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default;
+        naersk-lib = pkgs.naersk.override {
+          cargo = pkgs.rust-bin.nightly.latest.cargo;
+          rustc = rust;
+        };
+        rust-dev = rust.override {
+          extensions = [
+            "clippy-preview"
+            "rust-src"
+            "rustc-dev"
+            "rustfmt-preview"
+          ];
+        };
+      in rec {
+        # `nix build`
+        packages.kv-store-ninja = naersk-lib.buildPackage {
+          pname = "kv-store-ninja";
+          root = ./.;
+        };
+        defaultPackage = packages.kv-store-ninja;
+
+        # `nix run` or `nix run .#app`
+        apps.app = flake-utils.lib.mkApp {
+          drv = packages.kv-store-ninja;
+        };
+        defaultApp = apps.app;
+
+        # `nix run .#watch`
+        apps.watch = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellApplication {
+            name = "watch";
+            runtimeInputs = [
+              pkgs.cargo-watch
+              pkgs.gcc
+              rust
+            ];
+            text = ''
+              cargo-watch -w "./src/" -x "run"
+            '';
+          };
+        };
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cargo-edit
+            pkgs.cargo-watch
+            pkgs.rust-analyzer
+          ];
+          nativeBuildInputs = [ rust-dev ];
+        };
+      }
+    );
+}
