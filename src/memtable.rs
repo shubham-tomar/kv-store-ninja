8af87cc4284c75fe9865cb2230c19c104ff3bd0b
shubham.tomar <shubham.tomar@juspay.in>
2024-06-01 11:23:19 +0530

    init memtable struct

---
+++ src/memtable.rs
@@ -0,0 +1,19 @@
+#![allow(unused_imports, dead_code)]
+
+use skiplist::SkipMap;
+use std::sync::Arc;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+
+pub struct MemTableEntry {
+    pub key:       Vec<u8>,
+    pub value:     Vec<u8>,
+    pub timestamp: u128,
+    pub deleted:   bool
+}
+
+pub struct MemTable {
+    entries:  SkipMap<Vec<u8>, MemTableEntry>,
+    size:     usize,
+    max_size: usize
+}
