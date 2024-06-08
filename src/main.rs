00a721c4559b308dabee2964ed95047dea2cf7cb
shubham.tomar <shubham.tomar@juspay.in>
2024-06-09 00:38:37 +0530

    impl get,put,delete func

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,80 @@
 mod memtable;
 
+use clap::{Arg, Command};
+use crate::memtable::MemTable;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Key-Value Store")
+        .version("1.0")
+        .author("Your Name <youremail@example.com>")
+        .about("A simple key-value store using LSM tree principles")
+        .subcommand(Command::new("put")
+            .about("Inserts a key-value pair")
+            .arg(Arg::new("key")
+                .help("The key to insert")
+                .required(true)
+                .index(1))
+            .arg(Arg::new("value")
+                .help("The value to insert")
+                .required(true)
+                .index(2)))
+        .subcommand(Command::new("get")
+            .about("Retrieves a value by key")
+            .arg(Arg::new("key")
+                .help("The key to retrieve")
+                .required(true)
+                .index(1)))
+        .subcommand(Command::new("delete")
+            .about("Deletes a key")
+            .arg(Arg::new("key")
+                .help("The key to delete")
+                .required(true)
+                .index(1)))
+        .subcommand(Command::new("scan")
+            .about("Scans a range of keys")
+            .arg(Arg::new("start")
+                .help("The start key of the range")
+                .required(true)
+                .index(1))
+            .arg(Arg::new("end")
+                .help("The end key of the range")
+                .required(true)
+                .index(2)))
+        .get_matches();
+
+    let mut mem_table = MemTable::new(1000);
+
+    if let Some(matches) = matches.subcommand_matches("put") {
+        let key = matches.get_one::<String>("key").unwrap().as_bytes().to_vec();
+        let value = matches.get_one::<String>("value").unwrap().as_bytes().to_vec();
+        mem_table.put(key, value);
+        println!("Inserted key-value pair.");
+    }
+
+    if let Some(matches) = matches.subcommand_matches("get") {
+        let key = matches.get_one::<String>("key").unwrap().as_bytes().to_vec();
+        match mem_table.get(&key) {
+            Some(entry) if !entry.deleted => println!("Value: {:?}", entry.value),
+            _ => println!("Key not found or marked as deleted."),
+        }
+    }
+
+    if let Some(matches) = matches.subcommand_matches("delete") {
+        let key = matches.get_one::<String>("key").unwrap().as_bytes().to_vec();
+        mem_table.delete(&key);
+        println!("Key deleted.");
+    }
+
+    // if let Some(matches) = matches.subcommand_matches("scan") {
+    //     let start = matches.get_one::<String>("start").unwrap().as_bytes().to_vec();
+    //     let end = matches.get_one::<String>("end").unwrap().as_bytes().to_vec();
+        // let scan_result = mem_table.range_scan(&start, &end);
+        // for (key, entry) in scan_result {
+        //     if !entry.deleted {
+        //         println!("{:?}: {:?}", key, entry.value);
+        //     } else {
+        //         println!("{:?} is deleted", key);
+        //     }
+        // }
+    // }
 }
--- src/memtable.rs
+++ src/memtable.rs
@@ -4,7 +4,7 @@ use skiplist::SkipMap;
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-
+#[derive(Clone, Debug)]
 pub struct MemTableEntry {
     pub key:       Vec<u8>,
     pub value:     Vec<u8>,
@@ -26,6 +26,52 @@ impl MemTable {
             size: 0,
             max_size
         }
+    }
+
+    pub fn put(&mut self, key: Vec<u8>, value: Vec<u8>) {
+        let timestamp = Self::current_timestamp();
+        let entry = MemTableEntry {
+            key: key.clone(),
+            value: value.clone(),
+            timestamp,
+            deleted: false,
+        };
+        self.size += key.len() + value.len() + 16 + 1;
+        self.entries.insert(key, entry);
+        // println!("Entries {:?}", self.entries);
+        self.check_flush();
+    }
+
+    pub fn get(&self, key: &[u8]) -> Option<MemTableEntry> {
+        let v = self.entries.get(key).map(|entry| entry.clone());
+        // println!("Value: {:?}", v);
+        // println!("Entries {:?}", self.entries);
+        v
+    }
+
+    pub fn delete(&mut self, key: &[u8]) {
+        let timestamp = Self::current_timestamp();
+        let entry = MemTableEntry {
+            key: key.to_vec(),
+            value: Vec::new(),
+            timestamp,
+            deleted: true,
+        };
+        self.size += key.len() + 16 + 1; // Update size to include the length of the key, timestamp (16 bytes), and deleted flag (1 byte)
+        self.entries.insert(key.to_vec(), entry);
+        self.check_flush();
+    }
+
+    pub fn check_flush(&self) {
+        if self.size > self.max_size {
+            println!("Flushing memTable to SSTable....")
+            // Yet to be implemented
+        }
+    }
+
+    pub fn current_timestamp() -> u128 {
+        let now = SystemTime::now();
+        now.duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis()
     }
 
 
